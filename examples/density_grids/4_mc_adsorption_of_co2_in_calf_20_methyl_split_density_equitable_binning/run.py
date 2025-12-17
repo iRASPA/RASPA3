5937d8c45631f5c2d14759e9652bfda7429fa795
OliMarc <omarc104@uottawa.ca>
2025-12-17 16:25:44 -0500

    Added density grid MC adsorption example inputs
    Added example input files demonstrating the new density grid options.

---
+++ examples/density_grids/4_mc_adsorption_of_co2_in_calf_20_methyl_split_density_equitable_binning/run.py
@@ -0,0 +1,37 @@
+import raspalib
+
+
+ff = raspalib.ForceField("force_field.json")
+ff.useCharge = False
+
+mcmoves = raspalib.MCMoveProbabilities(translationProbability=0.5, reinsertionCBMCProbability=0.5, swapProbability=1.0)
+
+methane = raspalib.Component(
+    componentId=0,
+    forceField=ff,
+    componentName="methane",
+    fileName="methane.json",
+    particleProbabilities=mcmoves,
+)
+
+mfi = raspalib.Framework(
+    frameworkId=0,
+    forceField=ff,
+    componentName="MFI_SI",
+    fileName="MFI_SI.cif",
+    numberOfUnitCells=raspalib.int3(2, 2, 2),
+)
+
+system = raspalib.System(
+    systemId=0,
+    externalTemperature=300.0,
+    externalPressure=1e5,
+    forceField=ff,
+    components=[methane],
+    initialNumberOfMolecules=[0],
+    frameworkComponents=[mfi],
+)
+
+mc = raspalib.MonteCarlo(numberOfCycles=10000, numberOfInitializationCycles=2000, systems=[system], outputToFiles=True)
+
+mc.run()
