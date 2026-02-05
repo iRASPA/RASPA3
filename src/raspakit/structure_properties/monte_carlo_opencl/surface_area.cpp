ac2f3c857ef274e9a8e57dce8548834d7f9f4ad0
David Dubbeldam <d.dubbeldam@uva.nl>
2026-02-05 13:19:13 +0100

    feat: surface area gpu monte carlo gpu method

--- src/raspakit/structure_properties/monte_carlo_opencl/surface_area.cpp
+++ src/raspakit/structure_properties/monte_carlo_opencl/surface_area.cpp
@@ -18,6 +18,7 @@ module;
 #include <vector>
 #include <numeric>
 #include <chrono>
+#include <random>
 #endif
 
 #define CL_TARGET_OPENCL_VERSION 120
@@ -90,6 +91,7 @@ MC_OpenCL_SurfaceArea::MC_OpenCL_SurfaceArea()
 void MC_OpenCL_SurfaceArea::run(const ForceField &forceField, const Framework &framework, double wellDepthFactor, std::string probePseudoAtom,
                                 std::optional<std::size_t> numberOfIterations, std::optional<std::size_t> numberOfInnerSteps) const
 {
+  RandomNumber random{std::nullopt};
   cl_int err;
   std::chrono::system_clock::time_point time_begin, time_end;
   std::optional<std::size_t> probeType = forceField.findPseudoAtom(probePseudoAtom);
@@ -99,8 +101,8 @@ void MC_OpenCL_SurfaceArea::run(const ForceField &forceField, const Framework &f
     throw std::runtime_error(std::format("MC_SurfaceArea: Unknown probe-atom type\n"));
   }
 
-  std::size_t nummer_of_inner_steps = numberOfInnerSteps.value_or(1024);
-
+  std::size_t number_of_iterations = numberOfIterations.value_or(100);
+  std::size_t number_of_inner_steps = numberOfInnerSteps.value_or(1000);
 
   double3x3 unit_cell = framework.simulationBox.cell;
   double3x3 inverse_unit_cell = framework.simulationBox.inverseCell;
@@ -186,38 +188,70 @@ void MC_OpenCL_SurfaceArea::run(const ForceField &forceField, const Framework &f
   cl_float4 inverse_clCellc = {{cl_float(inverse_unit_cell[0][2]), cl_float(inverse_unit_cell[1][2]), cl_float(inverse_unit_cell[2][2]), cl_float(0.0)}};
 
   cl_int end_index = cl_int(numberOfAtoms);
-  cl_int number_of_slices = cl_int(nummer_of_inner_steps);
+  cl_int number_of_slices = cl_int(number_of_inner_steps);
 
   err = clSetKernelArg(surfaceAreaKernel, 0, sizeof(cl_mem), &inputPos);
   err |= clSetKernelArg(surfaceAreaKernel, 1, sizeof(cl_mem), &inputSigma);
-  err |= clSetKernelArg(surfaceAreaKernel, 2, sizeof(cl_mem), &outputArray);
-  err |= clSetKernelArg(surfaceAreaKernel, 3, sizeof(cl_int), &end_index);
-  err |= clSetKernelArg(surfaceAreaKernel, 4, sizeof(cl_int), &number_of_slices);
-  err |= clSetKernelArg(surfaceAreaKernel, 5, sizeof(cl_float4), &clCella);
-  err |= clSetKernelArg(surfaceAreaKernel, 6, sizeof(cl_float4), &clCellb);
-  err |= clSetKernelArg(surfaceAreaKernel, 7, sizeof(cl_float4), &clCellc);
-  err |= clSetKernelArg(surfaceAreaKernel, 8, sizeof(cl_float4), &inverse_clCella);
-  err |= clSetKernelArg(surfaceAreaKernel, 9, sizeof(cl_float4), &inverse_clCellb);
-  err |= clSetKernelArg(surfaceAreaKernel, 10, sizeof(cl_float4), &inverse_clCellc);
-  err |= clEnqueueNDRangeKernel(OpenCL::clCommandQueue.value(), surfaceAreaKernel, 1, nullptr, &global_work_size,
-                                &surfaceAreaWorkGroupSize, 0, nullptr, nullptr);
-  if (err != CL_SUCCESS)
+  err |= clSetKernelArg(surfaceAreaKernel, 3, sizeof(cl_mem), &outputArray);
+  err |= clSetKernelArg(surfaceAreaKernel, 4, sizeof(cl_int), &end_index);
+  err |= clSetKernelArg(surfaceAreaKernel, 5, sizeof(cl_int), &number_of_slices);
+  err |= clSetKernelArg(surfaceAreaKernel, 6, sizeof(cl_float4), &clCella);
+  err |= clSetKernelArg(surfaceAreaKernel, 7, sizeof(cl_float4), &clCellb);
+  err |= clSetKernelArg(surfaceAreaKernel, 8, sizeof(cl_float4), &clCellc);
+  err |= clSetKernelArg(surfaceAreaKernel, 9, sizeof(cl_float4), &inverse_clCella);
+  err |= clSetKernelArg(surfaceAreaKernel, 10, sizeof(cl_float4), &inverse_clCellb);
+  err |= clSetKernelArg(surfaceAreaKernel, 11, sizeof(cl_float4), &inverse_clCellc);
+
+  double accumulated_surface_area{};
+  for(std::size_t i = 0; i < number_of_iterations; ++i)
   {
-    throw std::runtime_error(std::format("OpenCL clEnqueueNDRangeKernel failed (err: {}) {} : {}\n", err, __FILE__, __LINE__));
-  }
+    // upload random Cartesian positions
+    std::size_t number_of_random_unit_vectors{number_of_inner_steps};
+    std::vector<cl_float4> random_unit_vectors(number_of_random_unit_vectors);
+    for (size_t j = 0; j < number_of_random_unit_vectors; j++)
+    {
+      double3 vec = random.randomVectorOnUnitSphere();
+      random_unit_vectors[j] = {{cl_float(vec.x), cl_float(vec.y), cl_float(vec.z), 0.0f}};
+    }
+    cl_mem random_unit_vectors_mem =
+          clCreateBuffer(OpenCL::clContext.value(), CL_MEM_READ_ONLY, sizeof(float4) *  random_unit_vectors.size(), nullptr, &err);
+    if (err != CL_SUCCESS)
+    {
+      throw std::runtime_error(std::format("OpenCL clCreateBuffer failed {} : {}\n", __FILE__, __LINE__));
+    }
+
+    err = clEnqueueWriteBuffer(OpenCL::clCommandQueue.value(), random_unit_vectors_mem, CL_TRUE, 0, sizeof(float4) * random_unit_vectors.size(),
+                                 random_unit_vectors.data(), 0, nullptr, nullptr);
+    if (err != CL_SUCCESS)
+    {
+      throw std::runtime_error(std::format("OpenCL clCommandQueue failed {} : {}\n", __FILE__, __LINE__));
+    }
+
+    err |= clSetKernelArg(surfaceAreaKernel, 2, sizeof(cl_mem), &random_unit_vectors_mem);
+    err |= clEnqueueNDRangeKernel(OpenCL::clCommandQueue.value(), surfaceAreaKernel, 1, nullptr, &global_work_size,
+                                  &surfaceAreaWorkGroupSize, 0, nullptr, nullptr);
+    if (err != CL_SUCCESS)
+    {
+      throw std::runtime_error(std::format("OpenCL clEnqueueNDRangeKernel failed (err: {}) {} : {}\n", err, __FILE__, __LINE__));
+    }
 
 
-  // Read the buffer back to the array
-  err = clEnqueueReadBuffer(OpenCL::clCommandQueue.value(), outputArray, CL_TRUE, 0, output.size() * sizeof(float),
-                            output.data(), 0, nullptr, nullptr);
-  if (err != CL_SUCCESS)
-  {
-    throw std::runtime_error("MC_OpenCL_SurfaceArea: error in clEnqueueReadBuffer");
+    // Read the buffer back to the array
+    err = clEnqueueReadBuffer(OpenCL::clCommandQueue.value(), outputArray, CL_TRUE, 0, output.size() * sizeof(float),
+                              output.data(), 0, nullptr, nullptr);
+    if (err != CL_SUCCESS)
+    {
+      throw std::runtime_error("MC_OpenCL_SurfaceArea: error in clEnqueueReadBuffer");
+    }
+
+    double surface_area = std::accumulate(output.begin(), output.end(), 0.0);
+    accumulated_surface_area += surface_area;
+
+    clReleaseMemObject(random_unit_vectors_mem);
   }
 
   clFinish(OpenCL::clCommandQueue.value());
 
-  double accumulated_surface_area = std::accumulate(output.begin(), output.end(), 0.0);
 
   time_end = std::chrono::system_clock::now();
 
@@ -227,14 +261,15 @@ void MC_OpenCL_SurfaceArea::run(const ForceField &forceField, const Framework &f
   myfile.open(framework.name + ".mc.sa.gpu.txt");
   std::print(myfile, "# Surface area using Monte Carlo-based method\n");
   std::print(myfile, "# Probe atom: {} well-depth-factor: {} sigma: {}\n", probePseudoAtom, wellDepthFactor, forceField[probeType.value()].sizeParameter());
+  std::print(myfile, "# Number of iterations: {}\n", number_of_iterations);
   std::print(myfile, "# Number of framework atoms: {}\n", framework.unitCellAtoms.size());
-  //std::print(myfile, "# Number of integration points per atom: {}\n", (numberOfSlices + 1) * numberOfSlices);
+  std::print(myfile, "# Number of inner-steps: {}\n", number_of_inner_steps);
   std::print(myfile, "# GPU Timing: {} [s]\n", timing.count());
-  myfile << accumulated_surface_area << " [A^2]" << std::endl;
-  myfile << accumulated_surface_area * Units::Angstrom * Units::Angstrom * Units::AvogadroConstant /
+  myfile << accumulated_surface_area / static_cast<double>(number_of_iterations)  << " [A^2]" << std::endl;
+  myfile << (accumulated_surface_area / static_cast<double>(number_of_iterations)) * Units::Angstrom * Units::Angstrom * Units::AvogadroConstant /
                 framework.unitCellMass
          << " [m^2/g]" << std::endl;
-  myfile << 1.0e4 * accumulated_surface_area / framework.simulationBox.volume << " [m^2/cm^3]" << std::endl;
+  myfile << 1.0e4 * (accumulated_surface_area / static_cast<double>(number_of_iterations)) / framework.simulationBox.volume << " [m^2/cm^3]" << std::endl;
 
   myfile.close();
 }
@@ -242,6 +277,7 @@ void MC_OpenCL_SurfaceArea::run(const ForceField &forceField, const Framework &f
 const char* MC_OpenCL_SurfaceArea::surfaceAreaKernelSource = R"foo(
 __kernel void ComputeSurfaceArea(__global float4 *position,
                                  __global float *sigma,
+                                 __global float4 *randomCartesianPositions,
                                  __global float *output,
                                  const int numberOfAtoms,
                                  const int numberOfSlices,
@@ -262,58 +298,46 @@ __kernel void ComputeSurfaceArea(__global float4 *position,
     float radius_i = sigma[iatom];
     float4 sphere_center = position[iatom];
 
-    for(int stackNumber = 0; stackNumber <= numberOfSlices; ++stackNumber)
+    for(int slice = 0; slice < numberOfSlices; ++slice)
     {
-      for(int sliceNumber = 0; sliceNumber < numberOfSlices; ++sliceNumber)
+      float4 unit_vector = randomCartesianPositions[slice];
+
+      // check overlap with other atoms
+      bool overlap = false;
+      for(int jatom = 0; jatom < numberOfAtoms; ++jatom)
       {
-        float theta = float(sliceNumber) * 2.0 * M_PI / float(numberOfSlices);
-
-        float u = float(stackNumber) / float(numberOfSlices);
-        float phi = acos(2.0 * u - 1.0);
-
-        float sinTheta = sin(theta);
-        float sinPhi = sin(phi);
-        float cosTheta = cos(theta);
-        float cosPhi = cos(phi);
-        float4 unit_vector = (float4)(sinPhi * cosTheta, sinPhi * sinTheta, cosPhi, 0.0f);
-
-        // check overlap with other atoms
-        bool overlap = false;
-        for(int jatom = 0; jatom < numberOfAtoms; ++jatom)
+        if(jatom != iatom)
         {
-          if(jatom != iatom)
+          float4 dr = (sphere_center + radius_i * unit_vector) - position[jatom];
+
+          ds.x = dot(inverse_cella, dr);
+          ds.y = dot(inverse_cellb, dr);
+          ds.z = dot(inverse_cellc, dr);
+          ds.w = 0.0f;
+
+          float4 t = ds - rint(ds);
+
+          dr.x = dot(cella, t);
+          dr.y = dot(cellb, t);
+          dr.z = dot(cellc, t);
+          dr.w = 0.0f;
+
+          float rr = dot(dr, dr);
+
+          if(rr < sigma[jatom] * sigma[jatom])
           {
-            float4 dr = (sphere_center + radius_i * unit_vector) - position[jatom];
-
-            ds.x = dot(inverse_cella, dr);
-            ds.y = dot(inverse_cellb, dr);
-            ds.z = dot(inverse_cellc, dr);
-            ds.w = 0.0f;
-
-            float4 t = ds - rint(ds);
-
-            dr.x = dot(cella, t);
-            dr.y = dot(cellb, t);
-            dr.z = dot(cellc, t);
-            dr.w = 0.0f;
-
-            float rr = dot(dr, dr);
-
-            if(rr < sigma[jatom] * sigma[jatom])
-            {
-              overlap = true;
-              break;
-            }
+            overlap = true;
+            break;
           }
         }
-
-        if(!overlap)
-        {
-          counted += 1.0f;
-        }
-
-        total += 1.0f;
       }
+
+      if(!overlap)
+      {
+        counted += 1.0f;
+      }
+
+      total += 1.0f;
     }
 
     output[ iatom ] = (counted / total) * 4.0 * M_PI * radius_i * radius_i;
