0ccdc281161200253732d1e0cfa80db5d38a3c74
David Dubbeldam <d.dubbeldam@uva.nl>
2026-05-23 14:56:10 +0200

    chore: build improvements

--- raspa/raspalib/__init__.py
+++ raspa/raspalib/__init__.py
@@ -124,13 +124,8 @@ def print_debug_info():
 
 
 from raspalib.version import __version__
-from raspalib.molecular_viewer import *
 
 __all__ = [
-    "print_debug_info",
-    "create_molecular_movie",
-    "MFI",
-    "ITQ_29",
     "PseudoAtom",
     "VDWParameters",
     "Atom",
